a9e4da30ee6b98a8ecca30d4bff3a65957829457
Alison Bento <alisonbnt@gmail.com>
2015-08-28 14:04:08 -0300

    Direct access to root prefix from API

--- SlimRestful/Prefix.php
+++ SlimRestful/Prefix.php
@@ -1,70 +1,70 @@
 <?php
-	
+
 namespace SlimRestful;
 
 class Prefix{
-	
+
 	private $slimRestfulInstance;
 	private $routePrefix;
 	private $resources;
 	private $middlewares;
-	
+
 	public function __construct($routePrefix){
 		$this->routePrefix = $routePrefix;
 		$this->resources = array();
 		$this->middlewares = array();
 	}
-	
-	public function getRoutePrefix(){
+
+	public function getRoute(){
 		return $this->routePrefix;
 	}
-	
+
 	public function setSlimRestfulInstance($srInstance){
 		$this->slimRestfulInstance = $srInstance;
 		return $this;
 	}
-	
+
 	public function getSlimRestfulInstance(){
 		return $this->slimRestfulInstance;
 	}
-	
+
 	public function addResource($route, Resource $resource){
 		$resource->setRoute($route);
 		$resource->setPrefix($this);
 		$this->resources[$route] = $resource;
 		return $resource;
 	}
-	
+
 	public function removeResource($route){
 		unset($this->resources[$route]);
 		return $this;
 	}
-	
+
 	public function addMiddleware($middleware){
 		$this->middlewares[] = $middleware;
 		return $this;
 	}
-	
+
 	public function preRun(){
 		foreach($this->resources as $resource){
 			$resource->mergeMiddlewares($this->middlewares);
 		}
 	}
-	
+
 	public function getResource($route){
 		return (isset($this->resources[$route]) ? $this->resources[$route] : null);
 	}
-	
+
 	public function getResources(){
 		return $this->resources;
 	}
-	
+
 	public function setRoutePrefix($routePrefix, $updateSlimRestfulInstance = true){
 		if($updateSlimRestfulInstance){
-			$this->slimRestfulInstance->changeRoutePrefix($this->routePrefix, $routePrefix, false);	
+			$this->slimRestfulInstance->changeRoutePrefix($this->routePrefix, $routePrefix, false);
 		}
 		$this->routePrefix = $routePrefix;
 		return $this;
 	}
-	
+
 }
